f1cd8b9e8c4c0c99c651e96284bbc3950a519ec5
dipstech11 <deepak.yadav@xoriant.com>
2019-05-17 11:50:01 +0530

    model er diagram made, static and media settings set up

--- videosubscription/videosubscription/settings.py
+++ videosubscription/videosubscription/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'memberships'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static_root")
+]
+
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
