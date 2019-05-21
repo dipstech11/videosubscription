3abf8d3cb80e5736d48d76494a48cdd8bc303543
dipstech11 <deepak.yadav@xoriant.com>
2019-05-21 14:41:44 +0530

    stripe setting set up payload and all

--- videosubscription/memberships/views.py
+++ videosubscription/memberships/views.py
@@ -78,6 +78,23 @@ def PaymentView(request):
 
     publishKey = settings.STRIPE_PUBLISHABLE_KEY
 
+    if request.method == "POST":
+        try:
+            token = request.POST['stripeToken']
+            stripe.Subscription.create(
+                customer= user_membership.stripe_customer_id,
+                items= [
+                {
+                "plan":selected_membership.stripe_plan_id,
+                },
+                
+                ],source=token
+            )
+
+        except stripe.CardError as e:
+            messages.info(request,"Your card has been declined")
+
+
     context= {
     'publishKey': publishKey,
     'selected_membership':selected_membership
